6e9e4be71957c52b71ab1aafb799f84b8a35e5a0
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-09 17:35:17 +0000

    table working

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -14,7 +14,6 @@ class Account
 
     def deposit(amount)
        @balance += amount 
-        # puts "You made a deposit of #{'%.2f' % amount} at #{@time.strftime("%d/%m/%Y")}" 
        @transaction_history.push("#{@time.strftime("%d/%m/%Y")}  || #{'%.2f' % amount} || || #{'%.2f' % @balance}")
        @balance 
     end 
@@ -22,24 +21,17 @@ class Account
     def withdraw(amount)
         @balance -= amount
         fail "You have insufficent funds" if @balance - amount < 0
-        # puts "You made a withdrawal of #{'%.2f' % amount} at #{@time.strftime("%d/%m/%Y")}"
         @transaction_history.push("#{@time.strftime("%d/%m/%Y")} || #{'%.2f' % amount} || || #{'%.2f' % @balance}")
         @balance
     end
 
-
-
+    
     def bank_statement
         @transaction_history << HEADER
-        @transaction_history.reverse.join("\n") do |value| 
-        print value 
-       end
+        printed_statement = @transaction_history.reverse.join("\n") 
+        puts printed_statement
+        return printed_statement 
+        
+        
     end
-
-    
 end 
-
- 
-#             14/01/2012 || || 500.00 || 2500.00
-#             13/01/2012 || 2000.00 || || 3000.00
-#             10/01/2012 || 1000.00 || || 1000.00"
--- lib/bank_statement.rb
+++
@@ -1,5 +0,0 @@
-# class Statement
-#     def bank_statement 
-#         "Date || Credit || Debit || Balance\n8/3/2021 || 10 || || 10"
-#     end 
-# end
