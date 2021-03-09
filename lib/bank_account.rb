39e438033687773dec942965fd2de0713922e99f
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-09 10:42:04 +0000

    bank statement method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,14 +13,21 @@ class Account
 
     def deposit(amount)
        @transaction_history << @balance += amount 
-       puts "You made a deposit of £#{amount} at #{@time}" 
+       puts "You made a deposit of £#{'%.2f' % amount} at #{@time.strftime("%d/%m/%Y")}" 
        @balance
     end 
 
     def withdraw(amount)
         @transaction_history << @balance -= amount
         fail "You have insufficent funds" if @balance - amount < 0
-        puts "You made a withdrawal of £#{amount} at #{@time}"
+        puts "You made a withdrawal of £#{'%.2f' % amount} at #{@time.strftime("%d/%m/%Y")}"
         @balance
+    end
+
+    def bank_statement
+       "date || credit || debit || balance
+            14/01/2012 || || 500.00 || 2500.00
+            13/01/2012 || 2000.00 || || 3000.00
+            10/01/2012 || 1000.00 || || 1000.00"
     end
 end 
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,6 +1,5 @@
 require 'bank_account'
 
-
 describe Account do 
 subject(:account) {described_class.new}
 
@@ -15,6 +14,7 @@ subject(:account) {described_class.new}
         it 'should be able to take a deposit' do 
             expect(subject).to respond_to(:deposit).with(1).argument
             expect(subject.deposit(10)).to eq(10)
+            
         end 
     end
 
@@ -27,4 +27,13 @@ subject(:account) {described_class.new}
             expect{ subject.withdraw(20) }.to raise_error "You have insufficent funds"
         end
     end
+
+    describe '#bank_statement' do 
+        it 'should print a bank statement' do 
+            expect(subject.bank_statement).to eq "date || credit || debit || balance
+            14/01/2012 || || 500.00 || 2500.00
+            13/01/2012 || 2000.00 || || 3000.00
+            10/01/2012 || 1000.00 || || 1000.00"
+        end
+    end 
 end 
