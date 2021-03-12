7224d154f59ce7adfa776875bf93b99669776efc
megan gosnell <megan_gosnell@aol.co.uk>
2021-03-12 11:52:22 +0000

    throws error if number is invalid

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,32 +1,29 @@
-# frozen_string_literal: true
-
 class Account
-  attr_reader :transaction_history, :balance, :time
-
-  DEFAULT_BALANCE = 0
-  HEADER = 'Date || Credit || Debit || Balance'
+attr_reader :balance, :time
+DEFAULT_BALANCE = 0
+  HEADER = "Date || Credit || Debit || Balance"
   def initialize
     @transaction_history = []
     @balance = DEFAULT_BALANCE
     @time = Time.new
   end
-
+  
   def bank_account
     @balance
   end
 
   def deposit(amount)
     @balance += amount
-    @transaction_history.push("#{@time.strftime('%d/%m/%Y')}  || #{'%.2f' % amount} || || #{format('%.2f',
-                                                                                                   @balance)}")
+    fail "This is not a valid number" unless amount.is_a?(Integer) && amount.positive?
+    @transaction_history.push("#{@time.strftime("%d/%m/%Y")}  || #{'%.2f' % amount} || || #{'%.2f' % @balance}")
     @balance
   end
 
   def withdraw(amount)
     @balance -= amount
-    raise 'You have insufficent funds' if (@balance - amount).negative?
-
-    @transaction_history.push("#{@time.strftime('%d/%m/%Y')} || #{'%.2f' % amount} || || #{format('%.2f', @balance)}")
+    fail "You have insufficent funds" if @balance - amount < 0
+    fail "This is not a valid number" unless amount.is_a?(Integer) && amount.positive?
+    @transaction_history.push("#{@time.strftime("%d/%m/%Y")} || #{'%.2f' % amount} || || #{'%.2f' % @balance}")
     @balance
   end
 
@@ -34,6 +31,6 @@ class Account
     @transaction_history << HEADER
     printed_statement = @transaction_history.reverse.join("\n")
     puts printed_statement
-    printed_statement
+    return printed_statement
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,38 +1,46 @@
-# frozen_string_literal: true
-
 require 'bank_account'
-
-describe Account do
-  subject(:account) { described_class.new }
-
-  context 'New Account'
-  describe '#initialize' do
-    it 'should create an instance' do
-      expect(subject.bank_account).to eq(0)
+describe Account do 
+subject(:account) {described_class.new}
+context 'New Account'
+    describe '#initialize' do 
+        it 'should create an instance' do 
+            expect(subject.bank_account).to eq(0)
+        end 
     end
-  end
 
-  describe '#deposit' do
-    it 'should be able to take a deposit' do
-      expect(subject.deposit(10)).to eq(10)
+    describe '#deposit' do 
+        it 'should be able to take a deposit' do 
+            expect(subject.deposit(10)).to eq(10)
+        end 
+        
+        it 'throws an error if you enter a negative amount' do 
+            expect{ subject.deposit(-1) }.to raise_error "This is not a valid number"
+        end 
     end
-  end
 
-  describe '#withdraw' do
-    it 'should be able to withdraw money' do
-      account.deposit(10)
-      expect(subject.withdraw(5))
-      expect(subject.bank_account).to eq(5)
-      expect { subject.withdraw(20) }.to raise_error 'You have insufficent funds'
-    end
-  end
+    describe '#withdraw' do 
+        it 'should be able to withdraw money' do
+            account.deposit(10) 
+            expect(subject.withdraw(5))
+            expect(subject.bank_account).to eq(5)
+        end
+        
+        it 'throws an error if their are insufficient funds' do
+            expect{ subject.withdraw(20) }.to raise_error "You have insufficent funds"
+        end
 
-  describe '#bank_statement' do
-    it 'should print a bank statement' do
-      subject.deposit(1000)
-      subject.deposit(2000)
-      subject.withdraw(500)
-      expect(subject.bank_statement).to eq("Date || Credit || Debit || Balance\n#{subject.time.strftime('%d/%m/%Y')} || 500.00 || || 2500.00\n#{subject.time.strftime('%d/%m/%Y')}  || 2000.00 || || 3000.00\n#{subject.time.strftime('%d/%m/%Y')}  || 1000.00 || || 1000.00")
+        it 'throws an error if you withdraw a negative amount' do 
+            expect{ subject.withdraw(-1) }.to raise_error "This is not a valid number"
+        end 
+    
     end
-  end
-end
+
+    describe '#bank_statement' do 
+        it 'should print a bank statement' do 
+            subject.deposit(1000)
+            subject.deposit(2000)
+            subject.withdraw(500)
+            expect(subject.bank_statement).to eq("Date || Credit || Debit || Balance\n#{subject.time.strftime('%d/%m/%Y')} || 500.00 || || 2500.00\n#{subject.time.strftime('%d/%m/%Y')}  || 2000.00 || || 3000.00\n#{subject.time.strftime('%d/%m/%Y')}  || 1000.00 || || 1000.00")
+        end
+    end 
+end 
